e9ff3fde1a4fbf8423259f98a20af07d83e38ab8
Coder-Artist <52375055+LeafGreenRemake@users.noreply.github.com>
2025-12-15 09:55:32 +0200

    adding classroom item and task item

--- app/src/main/java/com/example/student_group_manager/ClassroomsScreenActivity.kt
+++ app/src/main/java/com/example/student_group_manager/ClassroomsScreenActivity.kt
@@ -24,6 +24,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 
+private lateinit var auth: FirebaseAuth
 
 
 class ClassroomsScreenActivity : AppCompatActivity() {
