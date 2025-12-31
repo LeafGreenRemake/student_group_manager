414a9ae99896cf3dcae088193566c5426482a817
Coder-Artist <52375055+LeafGreenRemake@users.noreply.github.com>
2026-01-01 00:23:46 +0200

    screen glow up

--- app/src/main/java/com/example/student_group_manager/data/Student.kt
+++ app/src/main/java/com/example/student_group_manager/data/Student.kt
@@ -3,5 +3,5 @@ package com.example.student_group_manager.data
 data class Student (
     var id: String = "",
     val name: String = "",
-    val classrooms: Map<String, String>? = null
+    val classrooms: Map<String, String> = mapOf()
 )
