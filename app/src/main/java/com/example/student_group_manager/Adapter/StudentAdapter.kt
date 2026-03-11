5fc7cc3fb318ca69e47ffbcf95f045b3420de980
Coder-Artist <52375055+LeafGreenRemake@users.noreply.github.com>
2026-03-12 01:04:50 +0200

    added student removal dialog

--- app/src/main/java/com/example/student_group_manager/Adapter/StudentAdapter.kt
+++ app/src/main/java/com/example/student_group_manager/Adapter/StudentAdapter.kt
@@ -1,8 +1,10 @@
 package com.example.student_group_manager.Adapter
 
+import android.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.student_group_manager.R
@@ -12,6 +14,7 @@ class StudentAdapter(private val students: MutableList<Student>) : RecyclerView.
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val nameTv: TextView = view.findViewById(R.id.student_name_tv)
+        val removeButton: Button = view.findViewById(R.id.remove_student)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -22,6 +25,19 @@ class StudentAdapter(private val students: MutableList<Student>) : RecyclerView.
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val student = students[position]
         holder.nameTv.text = student.name
+
+        holder.removeButton.setOnClickListener {
+            val context = holder.itemView.context
+
+            AlertDialog.Builder(context)
+                .setMessage("למחוק את התלמיד מהכיתה?")
+                .setPositiveButton("כן") { _, _ ->
+                    
+                }
+
+                .setNegativeButton("לא", null)
+                .show()
+        }
     }
 
     override fun getItemCount(): Int = students.size
